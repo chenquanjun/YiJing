f4df71c63046f6f71cacbcffe54ac2657953306e
chenquanjun <chenshiyouxiang@163.com>
2014-04-02 22:38:32 +0800

    add

---
+++ scripts/app/objects/Background.lua
@@ -0,0 +1,107 @@
+require "app/objects/extern"
+
+--此处继承CCNode,因为需要维持这个表，但是用object的话需要retian/release
+Background = class("Background", function()
+return CCNode:create()
+end)
+
+Background.__index = Background
+
+local _bgSpriteVec = {}
+
+function Background:create()
+	local ret = Background.new()
+		ret:init()
+	return ret
+end
+
+function Background:init()
+    self:initBackground()
+    self:initBtn()
+end
+
+function Background:initBackground()
+     do --早上课室
+        local bg = display.newSprite("back_class_normal.png")
+        -- bg:setVisible(false)
+        bg:setPosition(ccp(display.cx, display.cy))
+        self:addChild(bg)
+        _bgSpriteVec[1] = bg
+    end
+
+    do --下午课室
+        local bg = display.newSprite("back_class_sunset.png")
+        bg:setVisible(false)
+        bg:setPosition(ccp(display.cx, display.cy))
+        self:addChild(bg)
+        _bgSpriteVec[2] = bg
+    end
+
+    do --晚上房间
+        local bg = display.newSprite("back_room_night.png")
+        bg:setVisible(false)
+        bg:setPosition(ccp(display.cx, display.cy))
+        self:addChild(bg)
+        _bgSpriteVec[3] = bg
+    end
+
+    do --早上房间
+        local bg = display.newSprite("back_room_normal.png")
+        bg:setVisible(false)
+        bg:setPosition(ccp(display.cx, display.cy))
+        self:addChild(bg)
+        _bgSpriteVec[4] = bg
+    end
+
+    do --下午房间
+        local bg = display.newSprite("back_room_sunset.png")
+        bg:setVisible(false)
+        bg:setPosition(ccp(display.cx, display.cy))
+        self:addChild(bg)
+        _bgSpriteVec[5] = bg
+    end
+   
+end
+
+function Background:initBtn()
+
+        local index = 1
+        local bgVec = _bgSpriteVec
+        local size = table.getn(bgVec)
+
+        local sprite = display.newSprite("refresh.png")
+        self:addChild(sprite)
+        -- sprite:setPosition(ccp(display.cx, display.cy))
+        sprite:setPosition(ccp(display.right - 100, display.top - 100))
+        sprite:setTouchEnabled(true) -- enable sprite touch
+        sprite:addTouchEventListener(function(event, x, y)
+            -- event: began, moved, ended
+            -- x, y: world coordinate
+            if event == "began" then
+                sprite:setScale(0.9)
+                return true -- catch touch event, stop event dispatching
+            end
+
+            local touchInSprite = sprite:getCascadeBoundingBox():containsPoint(CCPoint(x, y))
+            if event == "moved" then
+
+            elseif event == "ended" then
+                local bgPre = bgVec[index]
+                bgPre:setVisible(false)
+                index = index + 1
+                if index == size + 1 then
+                    index = 1
+                end
+                local bgNow = bgVec[index]
+                bgNow:setVisible(true) 
+
+                sprite:setScale(1.0)
+            else
+                sprite:setScale(1.0) 
+            end
+        end)   
+end
+
+
+function Background:onRelease()
+end
--- scripts/app/scenes/GameScene.lua
+++ scripts/app/scenes/GameScene.lua
@@ -1,13 +1,20 @@
 require "app/objects/Player"
+require "app/objects/Background"
 
 local GameScene = class("GameScene", function()
     return display.newScene("GameScene")
 end)
 
 function GameScene:ctor()
-    local label = ui.newTTFLabel({text = "少年の日常", size = 32, align = ui.TEXT_ALIGN_CENTER})
+    local label = ui.newTTFLabel({text = "1", size = 32, align = ui.TEXT_ALIGN_CENTER})
         :pos(display.cx, display.top - 50)
         :addTo(self, 10)
+
+    local bg = Background:create()
+
+    self:addChild(bg)
+
+
 
     -- do --player
     -- local player = self:newButton("player.png", function()
@@ -18,89 +25,89 @@ function GameScene:ctor()
     -- player:setPosition(ccp(display.cx - 50, display.cy)) 
     -- player:setScale(0.8)
     -- end
-    local player = Player:create()
+    -- local player = Player:create()
 
-    self:addChild(player, 10)
+    -- self:addChild(player, 10)
 
-    do --desk
-    local desk = self:newButton("desk.png", function()
-            print("ondesk")
-            label:setString("桌子")
-        end)    
-    self:addChild(desk)
-    desk:setPosition(ccp(display.cx, display.bottom + 60)) 
-    desk:setScale(0.8)
-    end
+    -- do --desk
+    -- local desk = self:newButton("desk.png", function()
+    --         print("ondesk")
+    --         label:setString("桌子")
+    --     end)    
+    -- self:addChild(desk)
+    -- desk:setPosition(ccp(display.cx, display.bottom + 60)) 
+    -- desk:setScale(0.8)
+    -- end
 
-    do --window
-    local window = self:newButton("window.png", function()
-            print("onwindow")
-            label:setString("窗子")
-        end)    
-    self:addChild(window)
-    window:setPosition(ccp(display.right - 200, display.top - 250)) 
-    window:setScale(0.8)
-    end
+    -- do --window
+    -- local window = self:newButton("window.png", function()
+    --         print("onwindow")
+    --         label:setString("窗子")
+    --     end)    
+    -- self:addChild(window)
+    -- window:setPosition(ccp(display.right - 200, display.top - 250)) 
+    -- window:setScale(0.8)
+    -- end
 
-    do --windowcolor
-    local windowcolor = self:newButton("windowcolor.png", function()
-            print("onwindowcolor")
-            -- label:setString("窗子")
-        end)    
-    self:addChild(windowcolor)
-    windowcolor:setPosition(ccp(display.right - 200, display.top - 250)) 
-    windowcolor:setScale(0.8)
-    end
+    -- do --windowcolor
+    -- local windowcolor = self:newButton("windowcolor.png", function()
+    --         print("onwindowcolor")
+    --         -- label:setString("窗子")
+    --     end)    
+    -- self:addChild(windowcolor)
+    -- windowcolor:setPosition(ccp(display.right - 200, display.top - 250)) 
+    -- windowcolor:setScale(0.8)
+    -- end
 
-    do --curtainOpen
-    local curtainOpen = self:newButton("curtain_open.png", function()
-            print("oncurtainOpen")
-            label:setString("窗帘打开")
-        end)    
-    self:addChild(curtainOpen)
-    curtainOpen:setPosition(ccp(display.right - 225, display.top - 150)) 
-    curtainOpen:setScale(0.8)
-    end
+    -- do --curtainOpen
+    -- local curtainOpen = self:newButton("curtain_open.png", function()
+    --         print("oncurtainOpen")
+    --         label:setString("窗帘打开")
+    --     end)    
+    -- self:addChild(curtainOpen)
+    -- curtainOpen:setPosition(ccp(display.right - 225, display.top - 150)) 
+    -- curtainOpen:setScale(0.8)
+    -- end
 
-    do --curtainClose
-    local curtainClose = self:newButton("curtain_close.png", function()
-            print("oncurtainClose")
-            label:setString("窗帘打开")
-        end)    
-    self:addChild(curtainClose)
-    curtainClose:setPosition(ccp(display.right - 225, display.top - 250)) 
-    curtainClose:setScale(0.8)
-    end
+    -- do --curtainClose
+    -- local curtainClose = self:newButton("curtain_close.png", function()
+    --         print("oncurtainClose")
+    --         label:setString("窗帘打开")
+    --     end)    
+    -- self:addChild(curtainClose)
+    -- curtainClose:setPosition(ccp(display.right - 225, display.top - 250)) 
+    -- curtainClose:setScale(0.8)
+    -- end
 
-    do --bookshelf
-    local bookshelf = self:newButton("bookshelf.png", function()
-            print("onbookshelf")
-            label:setString("书柜")
-        end)    
-    self:addChild(bookshelf)
-    bookshelf:setPosition(ccp(display.left + 400, display.bottom + 300)) 
-    bookshelf:setScale(0.8)
-    end
+    -- do --bookshelf
+    -- local bookshelf = self:newButton("bookshelf.png", function()
+    --         print("onbookshelf")
+    --         label:setString("书柜")
+    --     end)    
+    -- self:addChild(bookshelf)
+    -- bookshelf:setPosition(ccp(display.left + 400, display.bottom + 300)) 
+    -- bookshelf:setScale(0.8)
+    -- end
 
-    do --book
-    local book = self:newButton("book.png", function()
-            print("onbook")
-            label:setString("书")
-        end)    
-    self:addChild(book)
-    book:setPosition(ccp(display.left + 325, display.bottom + 250)) 
-    book:setScale(0.8)
-    end
+    -- do --book
+    -- local book = self:newButton("book.png", function()
+    --         print("onbook")
+    --         label:setString("书")
+    --     end)    
+    -- self:addChild(book)
+    -- book:setPosition(ccp(display.left + 325, display.bottom + 250)) 
+    -- book:setScale(0.8)
+    -- end
 
-    do --bookcolor
-    local bookcolor = self:newButton("bookcolor.png", function()
-            print("onbookcolor")
-            label:setString("书")
-        end)    
-    self:addChild(bookcolor)
-    bookcolor:setPosition(ccp(display.left + 80, display.bottom + 280)) 
-    bookcolor:setScale(0.8)
-    end
+    -- do --bookcolor
+    -- local bookcolor = self:newButton("bookcolor.png", function()
+    --         print("onbookcolor")
+    --         label:setString("书")
+    --     end)    
+    -- self:addChild(bookcolor)
+    -- bookcolor:setPosition(ccp(display.left + 80, display.bottom + 280)) 
+    -- bookcolor:setScale(0.8)
+    -- end
 
 end
 
